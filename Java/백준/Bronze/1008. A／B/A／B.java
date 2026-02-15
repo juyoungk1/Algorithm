89872e952d15063a1de8b638edead50ce04d959f
doodlios <soakaeofhg@naver.com>
2026-02-15 22:57:40 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17804 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double A = input.nextInt();
+        double B = input.nextInt();
+
+        double div = A/B;
+        System.out.println(div);
+
+
+
+    }
+}
