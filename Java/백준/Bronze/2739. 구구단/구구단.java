065a0c8134b52728bb71adf00e3704ebde647ab8
doodlios <soakaeofhg@naver.com>
2026-02-16 22:48:18 +0900

    [Bronze V] Title: 구구단, Time: 188 ms, Memory: 18488 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for (int i=N;i<=N;i++){
+            for (int j=1;j<=9;j++){
+                System.out.println(i+" * "+j+" = " + (i*j));
+            }
+
+        }
+
+
+
+
+
+    }
+}
