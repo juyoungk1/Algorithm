3d19f0da758be7504dd008cf53d7e38a4aba9159
doodlios <soakaeofhg@naver.com>
2026-02-17 14:56:03 +0900

    [Bronze V] Title: 등록, Time: 88 ms, Memory: 13812 KB -BaekjoonHub

---
+++ Java/백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int o=12;
+        String id="soakaeofhgg";
+
+        System.out.println(o);
+        System.out.println(id);
+}}
+
