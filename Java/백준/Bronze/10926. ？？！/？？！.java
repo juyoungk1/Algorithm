63e183c12fd7adb651b10357c80d182f42148d59
doodlios <soakaeofhg@naver.com>
2026-02-19 00:42:26 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String id = scanner.nextLine();
+
+        if (id.equals(id)) {
+            System.out.println(id+"??!");
+        }
+
+
+    }
+}
