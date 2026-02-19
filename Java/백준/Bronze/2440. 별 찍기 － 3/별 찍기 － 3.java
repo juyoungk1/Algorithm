660ddc0c001db38658b5a0d18ba79aeae3e83292
doodlios <soakaeofhg@naver.com>
2026-02-19 16:06:30 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 240 ms, Memory: 18004 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for (int i=1;i<=N;i++){
+            for (int j=0;j<=N-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
