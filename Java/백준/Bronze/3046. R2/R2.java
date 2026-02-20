a68eb94e3954f124d369db4f1dccffe954188182
doodlios <soakaeofhg@naver.com>
2026-02-20 14:33:36 +0900

    [Bronze IV] Title: R2, Time: 172 ms, Memory: 17764 KB -BaekjoonHub

---
+++ Java/백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int R1 = input.nextInt();
+        int S = input.nextInt();
+
+        int R2=(2*S)-R1;
+
+        System.out.println(R2);
+
+
+
+
+    }
+}
