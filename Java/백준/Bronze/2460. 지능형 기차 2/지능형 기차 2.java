82d81e0ebd62210a318ca681414b0dfbfe2b81ef
doodlios <soakaeofhg@naver.com>
2026-02-22 03:06:35 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 148 ms, Memory: 17696 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        int current=0;
+        int max=0;
+
+
+        for(int i=0;i<arr.length;i++){
+            int out=input.nextInt();
+            int in=input.nextInt();
+
+            current=current-out+in;
+
+            max=Math.max(max,current);
+        }
+
+        System.out.println(max);
+
+
+
+
+
+    }
+}
