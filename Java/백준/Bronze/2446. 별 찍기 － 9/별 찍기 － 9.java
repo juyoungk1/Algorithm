30bd36e5eee99a5a2db2c102f4966c14ee4cc693
doodlios <soakaeofhg@naver.com>
2026-02-22 02:36:39 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 436 ms, Memory: 24528 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for (int i=N;i>=1;i--){
+            for (int j=1;j<=N-i;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        for (int i=2;i<=N;i++){
+            for (int j=1;j<=N-i;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
