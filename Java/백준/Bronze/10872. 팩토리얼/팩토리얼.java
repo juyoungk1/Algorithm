d58f7f17c3e2ae07607cafe8eeb90e4c087d60ea
doodlios <soakaeofhg@naver.com>
2026-02-24 21:45:01 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17488 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N=input.nextInt();
+
+        int result=1;
+        for (int i = 1; i <=N; i++) {
+            result=result*i;
+        }
+        System.out.println(result);
+
+
+
+    }
+}
