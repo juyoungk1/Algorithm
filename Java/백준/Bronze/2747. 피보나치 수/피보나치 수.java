655482130d83afcd471aa0b363758785f1bd65da
doodlios <soakaeofhg@naver.com>
2026-03-06 22:20:44 +0900

    [Bronze II] Title: 피보나치 수, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n=input.nextInt();
+        
+
+        int sum=0;
+        int[] fibonacci=new int[46];
+        fibonacci[0]=0;
+        fibonacci[1]=1;
+        for(int i=2; i<=n; i++){
+            fibonacci[i]=fibonacci[i-1]+fibonacci[i-2];
+        }
+        System.out.println(fibonacci[n]);
+    }
+}
